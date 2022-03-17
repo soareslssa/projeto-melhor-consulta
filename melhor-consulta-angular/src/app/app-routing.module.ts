26eeccbce7c2fbca616617d9b71e30e584ab7495
fab_santanna <fab_santanna@hcpa.edu.br>
2022-03-17 13:54:58 -0300

    cria roteamento do modulo consultas

--- melhor-consulta-angular/src/app/app-routing.module.ts
+++ melhor-consulta-angular/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full',redirectTo: 'consultas'}
+  { path: '', pathMatch: 'full',redirectTo: 'consultas'},
+  { path: 'consultas',
+    loadChildren: () => import('./consultas/consultas.module').then(m => m.ConsultasModule)
+  }
 ];
 
 @NgModule({

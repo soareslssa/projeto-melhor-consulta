d38b38548b9bf717ca896bfdcaea985e4af25876
fab_santanna <fab_santanna@hcpa.edu.br>
2022-03-21 10:03:49 -0300

    cria projeto melhor-consulta-spring

---
+++ melhor-consulta-spring/src/main/java/com/soaresdev/melhorconsultaspring/MelhorConsultaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.soaresdev.melhorconsultaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelhorConsultaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelhorConsultaSpringApplication.class, args);
+	}
+
+}
---
+++ melhor-consulta-spring/src/test/java/com/soaresdev/melhorconsultaspring/MelhorConsultaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soaresdev.melhorconsultaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelhorConsultaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

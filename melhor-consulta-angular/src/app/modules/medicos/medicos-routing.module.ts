aeebd9a1cec73cff85bffc71c83032b9e28e5d4e
fab_santanna <fab_santanna@hcpa.edu.br>
2022-04-11 13:38:30 -0300

    exclui container de cadastro de medicos

--- melhor-consulta-angular/src/app/modules/medicos/medicos-routing.module.ts
+++ melhor-consulta-angular/src/app/modules/medicos/medicos-routing.module.ts
@@ -1,18 +1,8 @@
-import { ContatoMedicosComponent } from './containers/cadastrar-medicos/contato-medicos/contato-medicos.component';
-import { DadosProfissionaisMedicosComponent } from './containers/cadastrar-medicos/dados-profissionais-medicos/dados-profissionais-medicos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { CadastrarMedicosComponent } from '../medicos/containers/cadastrar-medicos/cadastrar-medicos.component';
-import { DadosPessoaisMedicosComponent } from './containers/cadastrar-medicos/dados-pessoais-medicos/dados-pessoais-medicos.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full',redirectTo: 'cadastrar/dados-pessoais-medicos'},
-  {path: 'cadastrar', component: CadastrarMedicosComponent,
-    children: [
-      {path: 'dados-pessoais-medicos',component: DadosPessoaisMedicosComponent},
-      {path: 'dados-profissionais-medicos',component: DadosProfissionaisMedicosComponent},
-      {path: 'contato-medicos',component: ContatoMedicosComponent}
-    ]}
+
 ];
 
 @NgModule({
--- melhor-consulta-angular/src/app/modules/medicos/medicos.module.ts
+++ melhor-consulta-angular/src/app/modules/medicos/medicos.module.ts
@@ -1,20 +1,13 @@
-import { DadosPessoaisMedicosComponent } from './containers/cadastrar-medicos/dados-pessoais-medicos/dados-pessoais-medicos.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { MedicosRoutingModule } from '../medicos/medicos-routing.module';
 import { SharedModule } from './../../shared/shared.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CadastrarMedicosComponent } from './containers/cadastrar-medicos/cadastrar-medicos.component';
-import { DadosProfissionaisMedicosComponent } from './containers/cadastrar-medicos/dados-profissionais-medicos/dados-profissionais-medicos.component';
-import { ContatoMedicosComponent } from './containers/cadastrar-medicos/contato-medicos/contato-medicos.component';
 
 
 
 @NgModule({
   declarations: [
-    CadastrarMedicosComponent,
-    DadosPessoaisMedicosComponent,
-    DadosProfissionaisMedicosComponent,
-    ContatoMedicosComponent
+
   ],
   imports: [
     CommonModule,

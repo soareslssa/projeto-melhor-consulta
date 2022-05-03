16bef32a6a6b8b463d4bb333f2640712441bd9ad
soareslss <soareslssa@fab.mil.br>
2022-05-03 10:48:13 -0300

    implementa funcionalidade de cadastro de usuarios

--- melhor-consulta-spring/src/main/java/com/soaresdev/melhorconsultaspring/repository/UsuarioRepository.java
+++ melhor-consulta-spring/src/main/java/com/soaresdev/melhorconsultaspring/repository/UsuarioRepository.java
@@ -1,13 +1,10 @@
 package com.soaresdev.melhorconsultaspring.repository;
 
-import com.soaresdev.melhorconsultaspring.models.GradeConsulta;
 import com.soaresdev.melhorconsultaspring.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
